9a0ca1edf84080a3d63348a926994eb34711573a
Anas Qazza <anasqazza@gmail.com>
2023-02-26 22:04:24 -0800

    askbcs shane3

--- models/Thought.js
+++ models/Thought.js
@@ -28,7 +28,31 @@ const reactionSchema = new Schema(
     id: false,
   }
 );
-
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: "You need to leave a thought!",
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
 });
